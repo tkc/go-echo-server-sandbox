4d46eb6a9828ce7964f81586edcf75bc093583f1
tkc <info@onga.jp>
2018-04-23 01:12:21 +0900

    Update handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
-	"strconv"
 	"net/http"
+	"github.com/spf13/cast"
 	"github.com/labstack/echo"
 	"github.com/tkc/go-echo-server-sandbox/models/user"
 )
@@ -11,35 +11,20 @@ type (
 	handler struct {
 		userModel userModel.User
 	}
-	resultStatus struct {
-		Status bool
-	}
-	resultUserJson struct {
-		User userModel.User
+	resultJson struct {
+		Id        int64
+		Name      string
+		Age       int
 	}
 	postUserData struct {
-		Name   string `json:"name" form:"name"`
-		Age    string `json:"age" form:"age"`
-		AgeInt int
+		Name   string `json:"Name" form:"name" validate:"required"`
+		Age    int `json:"Age" form:"age" validate:"required"`
 	}
-)
-
-/*
-	Template Type
-*/
-
-type (
-	userTemplate struct {
+	resultHtmlTemplate struct {
 		Title string
 		Users [] userModel.User
 	}
 )
-
-func (p *postUserData) changeAgeType() {
-	var i int
-	i, _ = strconv.Atoi(p.Age)
-	p.AgeInt = i
-}
 
 func CreateHandler(u userModel.User) *handler {
 	return &handler{u}
@@ -47,32 +32,47 @@ func CreateHandler(u userModel.User) *handler {
 
 func (h handler) GetTemplate(c echo.Context) error {
 	users := h.userModel.All(10, 0)
-	//users := userModel.User.All()//TODO
-	res := userTemplate{
+	res := resultHtmlTemplate{
 		Title: "users result",
 		Users: users,
 	}
 	return c.Render(http.StatusOK, "user", res)
 }
 
-func (h handler) GetUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	user := h.userModel.Fetch(int64(id))
-	return c.JSON(http.StatusOK, resultUserJson{User: user})
+func (h handler) Get(c echo.Context) error {
+	user := h.userModel.Fetch(cast.ToInt64(c.Param("id")))
+	return c.JSON(http.StatusOK, resultJson{
+		Id:user.Id,
+		Name:user.Name,
+		Age:user.Age,
+	})
 }
 
-func (h handler) CreateUser(c echo.Context) error {
+func (h handler) Create(c echo.Context) error {
 	post := new(postUserData)
 	if err := c.Bind(post); err != nil {
-		return err
+		return c.JSON(http.StatusOK, err.Error())
 	}
-	post.changeAgeType()
-	user := h.userModel.Create(post.Name, post.AgeInt)
-	return c.JSON(http.StatusOK, user)
+	if err := c.Validate(post); err != nil {
+		return c.JSON(http.StatusOK, err.Error())
+	}
+	user := h.userModel.Create(post.Name, post.Age)
+	return c.JSON(http.StatusOK, user.Id)
 }
 
-func (h handler) DeleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	h.userModel.Delete(int64(id))
-	return c.JSON(http.StatusOK, resultStatus{Status: true})
+func (h handler) Update(c echo.Context) error {
+	post := new(postUserData)
+	if err := c.Bind(post); err != nil {
+		return c.JSON(http.StatusOK, err.Error())
+	}
+	if err := c.Validate(post); err != nil {
+		return c.JSON(http.StatusOK, err.Error())
+	}
+	user := h.userModel.Create(post.Name, post.Age)
+	return c.JSON(http.StatusOK, user.Id)
+}
+
+func (h handler) Delete(c echo.Context) error {
+	h.userModel.Delete(cast.ToInt64(c.Param("id")))
+	return c.JSON(http.StatusOK, true)
 }
